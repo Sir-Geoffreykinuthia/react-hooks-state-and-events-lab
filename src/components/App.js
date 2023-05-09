c28447bc7109b9a201c26809f2757d8ea7dcfaf3
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-05-09 13:09:28 +0300

    Final Commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,21 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-   const [currentMode, setCurrentMode] = useState(false)
+   const [isColor, setColor] = useState(false)
 
-   function changeCurrentMode() {
-    setCurrentMode(!currentMode) 
-   }
+  function ChangeTheme() {
+    setColor(isColor => !isColor)
+  }
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = currentMode ? "App dark" : "App light"
+  const appClass = isColor ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button onClick={changeCurrentMode}>
-          {currentMode ? "Light Mode" : "Dark Mode"}
-          </button>
+          <button onClick={ChangeTheme}>{isColor ? "Dark Mode" : "Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
